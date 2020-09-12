043b1e6308039470db4fbe586531e3581c1e0133
jyyblue <jyyblue1987@outlook.com>
2020-09-12 16:28:34 +0300

    [fixed] center to screen.

--- MP3Uploader/Form1.Designer.cs
+++ MP3Uploader/Form1.Designer.cs
@@ -155,7 +155,8 @@
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnInstall);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UploaderConfig";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
