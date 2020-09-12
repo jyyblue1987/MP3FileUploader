f126e2d1384ab23d3671f3e79602ed48bd35d28f
jyyblue <jyyblue1987@outlook.com>
2020-09-12 13:41:20 +0300

    [create] create project.

---
+++ MP3Uploader/FileUploaderThread.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace MP3Uploader
+{
+    class FileUploaderThread
+    {
+        ftp ftpClient = null;
+
+        Boolean m_bRunning = true;
+        public void setThreadFlag(Boolean flag)
+        {
+            m_bRunning = flag;
+        }
+
+        public void run()
+        {
+            ftpClient = new ftp(@"ftp://192.168.0.110/", "test", "test");
+            
+            String app_data_path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            Console.WriteLine(app_data_path);
+
+            String record_path = app_data_path + "\\R";
+            while (m_bRunning)
+            {
+                try
+                {
+                    DateTime now = DateTime.Now;
+
+                    string[] files = Directory.GetFiles(record_path);
+                    foreach (string file in files)
+                    {                        
+                        String filename = Path.GetFileName(file);
+
+                        String upload_path = now.Year + "";
+                        ftpClient.createDirectory(upload_path);
+                        upload_path += "/" + now.Month;
+                        ftpClient.createDirectory(upload_path);
+                        upload_path += "/" + now.Day;
+                        ftpClient.createDirectory(upload_path);
+                        upload_path += "/" + filename;
+
+                        ftpClient.upload(upload_path, file);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Thread.Sleep(60 * 1000);
+            }
+            ftpClient = null;
+        }
+    }
+}
---
+++ MP3Uploader/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MP3Uploader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            testFTPUpload();
+            FileUploaderThread obj = new FileUploaderThread();
+            Thread thr = new Thread(new ThreadStart(obj.run));
+            thr.Start();
+        }
+
+        private void testFTPUpload()
+        {
+
+
+        }
+    }
+}
